9a618001a027fd56de2ba75eac50d987e30a527f
Dima Monin <monin.mitya@mail.ru>
2017-12-04 11:54:54 +0700

    Refactoring

--- src/test/java/TestAuthVKWithoutPO.java
+++ src/test/java/TestAuthVKWithoutPO.java
@@ -20,8 +20,8 @@ public class TestAuthVKWithoutPO {
 
     @BeforeClass
     public static void setUp() {
-//        System.setProperty("webdriver.gecko.driver", "./drivers/Mac/geckodriver");
-        System.setProperty("webdriver.gecko.driver", "./drivers/Unix/geckodriver");
+        System.setProperty("webdriver.gecko.driver", "./drivers/Mac/geckodriver");
+//        System.setProperty("webdriver.gecko.driver", "./drivers/Unix/geckodriver");
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         driver = new FirefoxDriver(capabilities);
 //        driver.manage().window().maximize();
